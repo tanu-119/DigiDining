7baeb73a540f094c71c8445f9c4d91fbec0b89bf
Devi Tanuja Veera <166426256+tanu-119@users.noreply.github.com>
2025-04-29 10:48:07 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 4173
-  },
   preview: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 4173
+    allowedHosts: ['digidining-1.onrender.com']
   }
 })
